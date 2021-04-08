4a4d47f483cbab50ca7daf20bb0521001055f789
cubOlson <cub.olson@gmail.com>
2021-04-08 18:06:25 -0400

    Setup done

--- tree.py
+++ tree.py
@@ -17,12 +17,12 @@ class Node:
             pass
         else:
             self._children.append(node)
-            node._parent = self
+            node.parent = self
 
     def remove_child(self, node):
         if node in self._children:
             self._children.remove(node)
-            node._parent = None
+            node.parent = None
         else:
             pass
 
@@ -32,5 +32,20 @@ class Node:
 
     @parent.setter
     def parent(self, node):
+        if self._parent == node:
+            return
+        if self._parent is not None:
+            self._parent.remove_child(self)
         self._parent = node
-        node.add_child(self)
+        if node is not None:
+            node.add_child(self)
+
+# node1 = Node("root1")
+# node2 = Node("root2")
+# node3 = Node("root3")
+
+# node3.parent = node1
+# node3.parent = node2
+
+# print(node1.children)
+# print(node2.children)
