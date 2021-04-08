10d027832c661c2e55365a4e4b1c1ab7610c4333
Marc Carlson <marc.carlson30@gmail.com>
2021-04-08 17:30:39 -0500

    completed phase 1

--- tree.py
+++ tree.py
@@ -40,12 +40,34 @@ class Node:
         if node is not None:
             node.add_child(self)
 
+    def depth_search(self, value):
+        stack = [self]
+        while len(stack):
+            current = stack.pop(-1)
+            if current._value == value:
+                return current
+            else:
+                stack.extend(current._children)
+
+    def breadth_search(self, value):
+        queue = [self]
+        while len(queue):
+            current = queue.pop(0)
+            if current._value == value:
+                return current
+            else:
+                queue.extend(current._children)
+
+
 # node1 = Node("root1")
 # node2 = Node("root2")
 # node3 = Node("root3")
 
 # node3.parent = node1
 # node3.parent = node2
+# node2.parent = node1
+# node3.parent = node2
 
+# print(node1.depth_search('root3').value)
 # print(node1.children)
 # print(node2.children)
