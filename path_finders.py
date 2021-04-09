4a4681da5ee442412a0fc3372c2c189568b8b4f4
cubOlson <cub.olson@gmail.com>
2021-04-09 12:05:41 -0400

    Done with phase 2

--- path_finders.py
+++ path_finders.py
@@ -3,24 +3,26 @@ from tree import Node
 class KnightPathFinder(Node):
     def __init__(self, coordinates):
         super().__init__(coordinates)
+        if coordinates < (0,0) or coordinates > (8,8):
+            self._root = Node((0.0))
         self._root = Node(coordinates)
-        self._considered_positions = {self._root}
+        self._considered_positions = {coordinates}
 
     def get_valid_moves(self, pos):
         valid = []
-        board = [(x,y) for x in range(0,8) for y in range(0,8)]   
+        board = [(x,y) for x in range(0,8) for y in range(0,8)]
         for square in board:
             res = tuple(map(lambda i, j: abs(i - j), pos, square))
             if res == (1,2) or res == (2,1):
                 valid.append(square)
         return valid
 
-# a = (10, 7)
-# b = (11, 5)
+    def new_move_positions(self, pos):
+            res = set(self.get_valid_moves(pos))
+            newMoves = res - self._considered_positions
+            self._considered_positions.update(res)
+            return newMoves
 
-# res = tuple(map(lambda i, j: abs(i - j), a, b))
 
-# print(res)
-
-node1 = KnightPathFinder((0,0))
-print(node1.get_valid_moves((2,2)))
+finder = KnightPathFinder((0, 0))
+print(finder.new_move_positions((0,0)))   # Expected outcome: {(1, 2), (2, 1)}
